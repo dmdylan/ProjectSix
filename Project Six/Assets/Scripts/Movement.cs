85be3b0c1ec06a78bc1fa2ee7e548e7ffb0bd1b1
dmdylan <dylandanowski@gmail.com>
2019-12-13 21:22:45 -0500

    Dungeon assests and camera/player movement script

---
+++ Project Six/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement(){
+        float forwardMovement;
+        float sideMovement;
+
+        forwardMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        sideMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(sideMovement, 0, forwardMovement);
+    }
+}
