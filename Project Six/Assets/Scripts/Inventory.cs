5d7db272dc6ff995bd2d20e2e27979bc2f03b030
dmdylan <dylandanowski@gmail.com>
2019-12-15 20:08:14 -0500

    Working on player input and inventory

---
+++ Project Six/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] playerInventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInventory = new GameObject[10];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AccessTheInventory()
+    {
+
+    }
+}
