1d2a589746ec66ab86b85cafd11d1d4e38f67ab0
dmdylan <dylandanowski@gmail.com>
2019-12-19 23:03:48 -0500

    Working out an inventory system

---
+++ Project Six/Assets/Scripts/Key.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu (menuName = "Items/Key", fileName = "KeyName.asset")]
+public class Key : Item
+{
+
+}
