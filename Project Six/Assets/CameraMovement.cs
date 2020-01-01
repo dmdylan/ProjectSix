e8823174f1421b87b2906e7ae3055fe3aac61ce3
dmdylan <dylandanowski@gmail.com>
2019-12-31 19:10:57 -0500

    Added new scene to test flight mechanics for different projcet

---
+++ Project Six/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ChaseCam();
+    }
+
+    private void ChaseCam()
+    {
+        float bias = 0.96f;
+        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f-bias);
+
+        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
+    }
+}
---
+++ Project Six/Assets/PlaneMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    public float planeSpeed = 90.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PreventPlayerFromGoingThroughTheGround();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerInputToMovePlane();
+    }
+
+    private void PlayerInputToMovePlane()
+    {
+        transform.position += transform.forward * Time.deltaTime * planeSpeed;
+
+        planeSpeed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+        if (planeSpeed < 35)
+            planeSpeed = 35;
+        if (planeSpeed > 110)
+            planeSpeed = 110;
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal") * 5f);        
+    }
+
+    private void PreventPlayerFromGoingThroughTheGround()
+    {
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if(terrainHeightWhereWeAre > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x,
+                                             terrainHeightWhereWeAre,
+                                             transform.position.z);
+        }
+    }
+}
